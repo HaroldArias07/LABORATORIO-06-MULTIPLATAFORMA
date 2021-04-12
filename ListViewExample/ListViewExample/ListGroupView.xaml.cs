636af4d79a6bf50f422f911510e315e6a692e986
Harold Arias <harold_joseph_07@hotmail.com>
2021-04-12 18:13:48 -0500

    first commit

---
+++ ListViewExample/ListViewExample/ListGroupView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListViewExample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListGroupView : ContentPage
+    {
+
+        ObservableCollection<User> users = new ObservableCollection<User>();
+
+        public ObservableCollection<User> Employees = new ObservableCollection<User>();
+
+        public ListGroupView()
+        {
+            InitializeComponent();
+            UserView.ItemsSource = users;
+            users.Add(new User { UserID = 1, UserName = "UserOne", GroupID = 1 });
+            users.Add(new User { UserID = 2, UserName = "UserTwo", GroupID = 1 });
+            users.Add(new User { UserID = 3, UserName = "UserThree", GroupID = 2 });
+            users.Add(new User { UserID = 4, UserName = "UserFour", GroupID = 1 });
+            users.Add(new User { UserID = 5, UserName = "UserFive", GroupID = 3 });
+            users.Add(new User { UserID = 6, UserName = "UserSix", GroupID = 3 });
+
+        }
+
+    }
+}
---
+++ ListViewExample/ListViewExample/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListViewExample
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public int GroupID { get; set; }
+    }
+}
